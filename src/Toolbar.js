9e277ec0d4c4701d75cd3478f5eceedffbe63312
Chris Harvey <chris@chrisnharvey.com>
2019-03-09 23:18:19 +0000

    Add initial toolbar component

---
+++ src/Toolbar.js
@@ -0,0 +1,22 @@
+import React, {Component} from 'react';
+import {FlatList, Image, StyleSheet, TouchableOpacity, View} from 'react-native';
+
+export default class Toolbar extends Component {
+  render() {
+    return (
+      <View
+          style={[{height: 50, backgroundColor: '#D3D3D3', alignItems: 'center'}, this.props.style]}
+      >
+        <FlatList
+            horizontal
+            keyExtractor={(item, index) => item.constructor.name + '-' + index}
+            data={React.Children.toArray(this.props.children)}
+            alwaysBounceHorizontal={false}
+            showsHorizontalScrollIndicator={false}
+            renderItem= {({item}) => item}
+        />
+      </View>
+    );
+  }
+
+}
