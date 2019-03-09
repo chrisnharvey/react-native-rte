7c9ae56938deb6e4caf0ff01a1deae0ef151ce4b
Chris Harvey <chris@chrisnharvey.com>
2019-03-09 23:30:57 +0000

    Add additional module exports

--- index.js
+++ index.js
@@ -1,3 +1,17 @@
 import RichText from './src/RichText'
+import Bold from './src/Actions/Bold'
+import Italic from './src/Actions/Italic';
+import OrderedList from './src/Actions/OrderedList';
+import UnorderedList from './src/Actions/UnorderedList';
+import Link from './src/Actions/Link';
+import Media from './src/Actions/Media';
 
-export default RichText
+module.exports = {
+    RichText,
+    Bold,
+    Italic,
+    OrderedList,
+    UnorderedList,
+    Link,
+    Media
+}
