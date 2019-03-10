1dd226ae76fdd157ca4c0e1284e27afb8e9ec3bb
Chris Harvey <chris@chrisnharvey.com>
2019-03-10 20:38:15 +0000

    Add highlighting of active tools in toolbar

--- src/Actions/Bold.js
+++ src/Actions/Bold.js
@@ -3,6 +3,10 @@ import { TouchableOpacity, Image } from 'react-native'
 import { EditorProxy } from '../Editor'
 
 export default class Bold extends Component {
+  state = {
+    active: false
+  }
+
   componentDidMount() {
     EditorProxy.current.addListener(this.editorEvent.bind(this))
   }
@@ -12,6 +16,7 @@ export default class Bold extends Component {
       <TouchableOpacity
         style={[
           {height: 50, width: 50, justifyContent: 'center'},
+          this.state.active ? {backgroundColor: 'red'} : {}
         ]}
         onPress={this.onPress.bind(this)}
       >
@@ -21,9 +26,27 @@ export default class Bold extends Component {
   }
 
   editorEvent(payload) {
+    if (payload.event == 'editor-change') {
+      this.triggerUpdate()
+    }
+  }
+
+  triggerUpdate() {
+    EditorProxy.current.getFormat().then((data) => {
+      if (data.bold) {
+        this.setState({
+          active: true
+        })
+      } else {
+        this.setState({
+          active: false
+        })
+      }
+    })
   }
 
   onPress() {
-    EditorProxy.current.trigger('format', 'bold', true)
+    EditorProxy.current.trigger('format', 'bold', !this.state.active)
+
   }
 }
--- src/Actions/UnorderedList.js
+++ src/Actions/UnorderedList.js
@@ -3,6 +3,10 @@ import { TouchableOpacity, Image } from 'react-native'
 import { EditorProxy } from '../Editor'
 
 export default class UnorderedList extends Component {
+  state = {
+    active: false
+  }
+
   componentDidMount() {
     EditorProxy.current.addListener(this.editorEvent.bind(this))
   }
@@ -12,6 +16,7 @@ export default class UnorderedList extends Component {
       <TouchableOpacity
         style={[
           {height: 50, width: 50, justifyContent: 'center'},
+          this.state.active ? {backgroundColor: 'red'} : {}
         ]}
         onPress={this.onPress.bind(this)}
       >
@@ -21,6 +26,23 @@ export default class UnorderedList extends Component {
   }
 
   editorEvent(payload) {
+    if (payload.event == 'editor-change') {
+      this.triggerUpdate()
+    }
+  }
+
+  triggerUpdate() {
+    EditorProxy.current.getFormat().then((data) => {
+      if (data.list == 'bullet') {
+        this.setState({
+          active: true
+        })
+      } else {
+        this.setState({
+          active: false
+        })
+      }
+    })
   }
 
   onPress() {
